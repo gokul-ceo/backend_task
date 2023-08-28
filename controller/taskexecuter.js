5f53d0b35e6755c7a35db46cc80d007fc2e53be3
Gokulg001 <gokuljune2003@gmail.com>
2023-08-28 23:44:21 +0530

    added new files

--- controller/taskexecuter.js
+++ controller/taskexecuter.js
@@ -1,5 +1,12 @@
 function questionFramer(task) {
     let question = '';
+    const validOperators = [
+        'plus', 'minus', 'by', 'into', 'sqrt', 'sin', 'cos', 'tan',
+        'abs', 'ceil', 'floor', 'round', 'log', 'exp', 'min', 'max',
+        'avg', 'sum', 'factorial', 'fibonacci', 'random', 'percentage',
+        'convert', 'gcd', 'atan', 'acos', 'asin', 'power', 'mod'
+    ];
+    const validUnitOperators = ['sqrt', 'sin', 'cos', 'tan', 'abs', ]
 
     for (let index = 0; index < task.length; index++) {
         const element = Number(task[index]);
@@ -18,7 +25,7 @@ function questionFramer(task) {
                     question += '/';
                     break;
                 case 'into':
-                    question += '*';  // Changed 'x' to '*'
+                    question += '*';  
                     break;
                 default:
                     break;
@@ -31,26 +38,37 @@ function questionFramer(task) {
 
 const Taskexecuter = (task) => {
     let result = Number(task[0]);
+    const validOperators = [
+        'plus', 'minus', 'by', 'into', 'sqrt', 'sin', 'cos', 'tan',
+        'abs', 'ceil', 'floor', 'round', 'log', 'exp', 'min', 'max',
+        'avg', 'sum', 'factorial', 'fibonacci', 'random', 'percentage',
+        'convert', 'gcd', 'atan', 'acos', 'asin', 'power', 'mod'
+    ];
 
     for (let index = 1; index < task.length; index += 2) {  // Process every second element as an operator
         const operator = task[index];
         const num = Number(task[index + 1]);
+        if (validOperators.includes(operator)){
+            switch (operator) {
+                case 'plus':
+                    result += num;
+                    break;
+                case 'minus':
+                    result -= num;
+                    break;
+                case 'by':
+                    result /= num;
+                    break;
+                case 'into':
+                    result *= num;
+                    break;
+                default:
+                    break;
+            }
 
-        switch (operator) {
-            case 'plus':
-                result += num;
-                break;
-            case 'minus':
-                result -= num;
-                break;
-            case 'by':
-                result /= num;
-                break;
-            case 'into':
-                result *= num;
-                break;
-            default:
-                break;
+        }
+        else{
+            console.log('Invalid operator');
         }
     }
 
